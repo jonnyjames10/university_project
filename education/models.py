63f85231fa206f3f452315583d7ccf6316471c3d
Jonathan Streatfeild-James <streatfeild-jamesjc@cardiff.ac.uk>
2023-02-18 23:10:58 +0000

    Change to models.py

--- education/models.py
+++ education/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hah = db.Column(db.String(128))
     date_of_birth = db.Column(db.DateTime, nullable=False)
     role = db.Column(db.String(10), nullable=False, default='stu')
 
