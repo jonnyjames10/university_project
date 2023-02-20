8ea96168b8d81f3fd62aa7a749e305adff861db7
Jonathan Streatfeild-James <streatfeild-jamesjc@cardiff.ac.uk>
2023-02-20 13:05:30 +0000

    Issue with models.py

---
+++ education/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from education import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    date_of_birth = db.Column(db.DateTime, nullable=False)
+    role = db.relationship('RoleMember', backref='user')
+
+    def __repr__(self):
+        return f"User('{self.id}', {self.email}')"
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(16), nullable=False)
+    user_role = db.relationship('RoleMember', backref='role')
+
+class RoleMember(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
