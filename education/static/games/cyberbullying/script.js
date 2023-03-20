b6cf377f5728da9bcfcda9d6686309d8f2c88c22
Jonathan Streatfeild-James <streatfeild-jamesjc@cardiff.ac.uk>
2023-03-20 18:07:54 +0000

    Sorted a bug that kept calling the unwanted update function and set values for setting the title and contents of questions

--- education/static/games/cyberbullying/script.js
+++ education/static/games/cyberbullying/script.js
@@ -8,46 +8,48 @@ const playerScoreElem = document.getElementById("player-score");
 const computerScoreElem = document.getElementById("computer-score");
 const modal = document.querySelector('#modal');
 const closeModal = document.querySelector('.close-button');
+const title = document.getElementById("title");
+const question = document.getElementById("question");
+const answers = document.getElementById("answers");
 
 let lastTime;
-let playing = true;
+let playing = false;
+title.innerHTML = "Welcome!"
+question.innerHTML = "Play pong and after every point, you will be asked a question.<br>You will be awarded points if you get the question correct.<br>The quicker you answer it correctly, the more points you'll get."
+modal.showModal();
 
 function update(time) {
     if (lastTime != null) {
         const delta = time - lastTime;
-        // ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
         computerPaddle.update(delta, ball.y);
         const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"));
 
         document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
-
-        if (isLose()) {
-            handleLose();
-            window.requestAnimationFrame(pause);
-        }
     }
-    
-    lastTime = time;
-    window.requestAnimationFrame(update);
+
+    if (isLose()) {
+        handleLose();
+        window.requestAnimationFrame(pause);
+    } else {    
+        lastTime = time;
+        window.requestAnimationFrame(update);
+    }
 }
 
 function pause(time) {
-    if (playing == true) {
-        window.requestAnimationFrame(update);
-    }
-
     if (lastTime != null) {
         const delta = time - lastTime;
-        computerPaddle.update(delta, ball.y);
-        playerPaddle.update(delta, ball.y);
         const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"));
 
         document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
-
     }
-    
-    lastTime = time;
-    window.requestAnimationFrame(pause);
+    if (playing == true) {
+        window.requestAnimationFrame(update);
+    } else {
+        lastTime = time;
+        window.requestAnimationFrame(pause);
+    }
 }
 
 function isLose() {
@@ -68,7 +70,14 @@ async function handleLose() {
     } else {
         computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1;
     }
-    window.requestAnimationFrame(pause);
+    //TODO:
+        // Pick random number
+        // Select the question from the selected list and the corresponding answer
+        // Set the innerHTML title, question and answers
+    title.innerHTML = "New question";
+    question.innerHTML = "The question will go here";
+        // Show the modal
+        // Need to sort the submit button - currently restarts the webpage
     modal.showModal(); // Pop up question for the answer
     ball.reset();
     computerPaddle.reset();
@@ -78,8 +87,4 @@ document.addEventListener("mousemove", e =>  {
     playerPaddle.position = (e.y / window.innerHeight) * 100;
 })
 
-if (playing == true) {
-    window.requestAnimationFrame(update);
-} else {
-    window.requestAnimationFrame(pause);
-}
+window.requestAnimationFrame(pause);
